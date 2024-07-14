68d05697fe3820e3895fe4358a77084c82076848
note-nausicaa <173652776+note-nausicaa@users.noreply.github.com>
2024-07-14 15:05:23 +1000

    Added calculator function and adjusted style of buttons

--- script.js
+++ script.js
@@ -1,11 +1,96 @@
-// Add click events for buttons
 
-const input = document.getElementById("input");
+//Display
+const userDisplay = document.querySelector("span");
 
-// User butttons
-const userButtons = document.querySelectorAll(".user");
+const numberButtons = document.querySelectorAll('.user');
+const button = document.querySelector('button');
 
-userButtons.forEach((userButton))
+let firstNumber = 0;
+let secondNumber = 0;
+
+// numberButtons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         if (userDisplay.textContent == ''){
+//             const number = button.textContent;
+//             firstNumber = parseFloat(number);
+//             userDisplay.textContent = number;
+//         }
+//         else {
+//             const userOperation = userDisplay.textContent;
+//             const number = button.textContent;
+//             secondNumber = parseFloat(number);
+//             let newDisplay = userOperation.concat(number);
+//             userDisplay.textContent = newDisplay;
+//         }
+//     })
+// });
+
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (userDisplay.textContent == ''){
+            const number = button.textContent;
+            userDisplay.textContent = number;
+        }
+        else {
+            const userOperation = userDisplay.textContent;
+            const number = button.textContent;
+            let newDisplay = userOperation.concat(number);
+            userDisplay.textContent = newDisplay;
+        }
+    })
+});
+
+
+
+const operatorButtons = document.querySelectorAll('.operation');
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const number = userDisplay.textContent;
+        const operator = button.textContent;
+        let newDisplay = number.concat(operator);
+        userDisplay.textContent = newDisplay;
+    })
+})
+
+const equals = document.getElementById('equals');
+
+equals.addEventListener('click', () => {
+    let result = userDisplay.textContent;
+    if (result.includes("+") == true) {
+        firstNumber = result.split('+').map((element) => Number(element)).reduce((total, addNum) => total += addNum)
+    } else if (result.includes("-") == true) 
+    {
+        firstNumber = firstNumber - secondNumber;
+    } else if (result.includes("/") == true) {
+        firstNumber = firstNumber / secondNumber;
+    }
+    else if (result.includes("*")== true) {
+        firstNumber = firstNumber*secondNumber;
+    }
+    userDisplay.textContent = firstNumber;
+})
+
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', () => {
+    userDisplay.textContent = '';
+    firstNumber = 0;
+    secondNumber = 0;
+})
+
+//
+
+/* const fourButton = document.getElementById("four");
+
+fourButton.addEventListener('click', () => {
+    let number = fourButton.textContent;
+    userDisplay.textContent = number;
+})*/
+
+
+//Maybe can add text value from HTML file to a string ??
 
 
 /*
@@ -28,7 +113,7 @@ function operate() {
     equalButton.addEventListener('click', () => {
 
     })
-} */
+} 
 
 const addButton = document.getElementById("add");
 
@@ -60,3 +145,4 @@ const divideButton = document.getElementById("divide");
 divideButton.addEventListener('click', () => {
     //Divide operation
 })
+    */
